fb6216dd6cf91d139d5096d0bb6e7e55ad384766
Saipavanganesh <pavanganeshms@gmail.com>
2023-01-08 01:47:17 +0530

    Valid Last Name

--- src/com/regex/Regex.java
+++ src/com/regex/Regex.java
@@ -4,16 +4,23 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Regex {
-    public static void main(String[] args) {
-        System.out.println("Enter first name");
-        Scanner sc = new Scanner(System.in);
-        String firstName = sc.next();
-        boolean pattern = Pattern.matches("[A-Z]{1}[a-z]{2,}$", firstName);
+    public static void checkPattern(String input, boolean pattern){
         if(pattern){
-            System.out.println("Valid First name");
+            System.out.println("Valid Pattern for " + input);
         }
         else{
-            System.out.println("Invalid First Name");
+            System.out.println("Invalid Pattern for " + input);
         }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first name");
+        String firstName = sc.next();
+        boolean firstNamePattern = Pattern.matches("[A-Z]{1}[a-z]{2,}$", firstName);
+        System.out.println("Enter last name");
+        String lastName = sc.next();
+        boolean lastNamePattern = Pattern.matches("[A-Z]{1}[a-z]{2,}$", lastName);
+        checkPattern(firstName, firstNamePattern);
+        checkPattern(lastName, lastNamePattern);
     }
 }
