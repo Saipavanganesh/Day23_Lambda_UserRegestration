dd16f6f87918014c63f53db2dbf03ee1b8aec344
Saipavanganesh <pavanganeshms@gmail.com>
2023-01-08 01:36:33 +0530

    Valid FirstName

--- src/com/regex/Regex.java
+++ src/com/regex/Regex.java
@@ -1,7 +1,19 @@
 package com.regex;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Regex {
     public static void main(String[] args) {
-        System.out.println("Regex");
+        System.out.println("Enter first name");
+        Scanner sc = new Scanner(System.in);
+        String firstName = sc.next();
+        boolean pattern = Pattern.matches("[A-Z]{1}[a-z]{2,}$", firstName);
+        if(pattern){
+            System.out.println("Valid First name");
+        }
+        else{
+            System.out.println("Invalid First Name");
+        }
     }
 }
