486443377e96864ec814f086d8e95d12a7bbe6fb
Saipavanganesh <pavanganeshms@gmail.com>
2023-01-08 02:03:28 +0530

    valid email

--- src/com/regex/Regex.java
+++ src/com/regex/Regex.java
@@ -14,13 +14,21 @@ public class Regex {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter first name");
         String firstName = sc.next();
-        boolean firstNamePattern = Pattern.matches("[A-Z]{1}[a-z]{2,}$", firstName);
+        boolean firstNamePattern = Pattern.matches("^[A-Z]{1}[a-z]{2,}$", firstName);
+
         System.out.println("Enter last name");
         String lastName = sc.next();
-        boolean lastNamePattern = Pattern.matches("[A-Z]{1}[a-z]{2,}$", lastName);
+        boolean lastNamePattern = Pattern.matches("^[A-Z]{1}[a-z]{2,}$", lastName);
+
+        System.out.println("Enter email");
+        String email =sc.next();
+        boolean emailPattern = Pattern.matches("^[A-Za-z0-9]+[.]?[A-Za-z0-9]+[@][a-zA-Z]+[.][a-zA-Z]+[.]?[a-zA-Z]{2}$", email);
+
         checkPattern(firstName, firstNamePattern);
         checkPattern(lastName, lastNamePattern);
+        checkPattern(email, emailPattern);
     }
 }
