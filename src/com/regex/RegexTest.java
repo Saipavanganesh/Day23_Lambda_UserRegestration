6370fc524d66b74ce1a82b378acf2b13c3fb4c47
Saipavanganesh <pavanganeshms@gmail.com>
2023-01-19 05:52:52 +0530

    exceptions added

--- src/com/regex/RegexTest.java
+++ src/com/regex/RegexTest.java
@@ -7,31 +7,31 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 class RegexTest {
     @Test
-    public void testFirstName(){
+    public void testFirstName() throws Exception{
         Regex regex = new Regex();
         String firstName = "Sai";
         Assertions.assertEquals("Valid Pattern for " + firstName, regex.firstName(firstName));
     }
     @Test
-    public void testLastName(){
+    public void testLastName() throws Exception{
         Regex regex = new Regex();
         String lastName = "Pavan";
         Assertions.assertEquals("Valid Pattern for " + lastName, regex.lastName(lastName));
     }
     @Test
-    public void testMobileNumber(){
+    public void testMobileNumber() throws Exception{
         Regex regex = new Regex();
         String mobileNumber = "8374218460";
         Assertions.assertEquals("Valid Pattern for " + mobileNumber, regex.mobileNumber(mobileNumber));
     }
     @Test
-    public void testEmail(){
+    public void testEmail() throws Exception{
         Regex regex = new Regex();
         String email = "saipavanganesh@email.com";
         Assertions.assertEquals("Valid Pattern for " + email, regex.email(email));
     }
     @Test
-    public void testPassword(){
+    public void testPassword() throws Exception{
         Regex regex = new Regex();
         String password = "Bridge@123";
         Assertions.assertEquals("Valid Pattern for " + password, regex.password(password));
